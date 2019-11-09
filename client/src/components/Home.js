1ec349694d8a7b6fe17178653e6c47dd2e278536
nolbuzanis <nolanbuzanis@gmail.com>
2019-11-09 10:01:05 -0500

    Turned url input into controlled component

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -2,35 +2,43 @@ import React from 'react';
 import './Home.css';
 import newspaperBG from './img/newspaper-background.jpg';
 
-const Start = props => {
-  return (
-    <div>
-      <div
-        className='hero-img'
-        style={{
-          background: `url(${newspaperBG}) center center no-repeat`,
-          backgroundSize: 'cover'
-        }}
-      >
-        <div className='hero-overlay'></div>
-      </div>
-      <div className='home-container'>
-        <h1>quizzical.</h1>
+class Start extends React.Component {
+  state = {
+    urlInput: ''
+  };
 
-        <h3>
-          (adj.) <br /> expressing slight uncertainty or amusement.
-        </h3>
-        <div className='input-container'>
-          <input
-            className='url-input'
-            type='text'
-            placeholder='https://www.buzzfeed.com/d29d…'
-          ></input>
-          <button className='start-quiz-button'>Take Quiz</button>
+  render() {
+    return (
+      <div>
+        <div
+          className='hero-img'
+          style={{
+            background: `url(${newspaperBG}) center center no-repeat`,
+            backgroundSize: 'cover'
+          }}
+        >
+          <div className='hero-overlay'></div>
+        </div>
+        <div className='home-container'>
+          <h1>quizzical.</h1>
+
+          <h3>
+            (adj.) <br /> expressing slight uncertainty or amusement.
+          </h3>
+          <div className='input-container'>
+            <input
+              className='url-input'
+              type='text'
+              placeholder='https://www.buzzfeed.com/d29d…'
+              value={this.state.urlInput}
+              onChange={e => this.setState({ urlInput: e.target.value })}
+            ></input>
+            <button className='start-quiz-button'>Take Quiz</button>
+          </div>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default Start;
