07bbe84005eab2e92272fe41e8089b7e58ede13b
nolbuzanis <nolanbuzanis@gmail.com>
2019-11-11 17:53:11 -0500

    Setting up run cmd and engines so that app is ready for heroku deployment

--- server.js
+++ server.js
@@ -13,6 +13,16 @@ app.get('/api/quiz', scraper);
 const emailer = require('./util/mailer');
 app.post('/email', emailer);
 
+if (process.env.NODE_ENV === 'production') {
+  // Serve any production assets like our main.css or main.js
+  app.use(express.static('client/build'));
+  // Handle all routing --> serve up index.html if it doesn't recognize the route
+  const path = require('path');
+  app.get('*', function(req, res) {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, (req, res, next) => {
